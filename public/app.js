88ab65e715384ff7b0a3c23726c98de2818e6e0e
Hamid Rasool <hamidrasool1912@gmail.com>
2023-09-20 02:41:14 +0500

    Added structure for AJAX requests

--- public/app.js
+++ public/app.js
@@ -1,3 +1,73 @@
+const API_METHODS = ["GET", "POST", "PUT", "DELETE"];
+
 const app = {};
 
-console.log("Right now consling from front end");
+app.config = {
+  session: false,
+};
+
+app.config.request = function(path, method, queryStringObject, payload, headers, callback) {
+  // Validation and Defaults
+  path = typeof path === "string" ? path : "";
+  method = typeof method === "string" && API_METHODS.includes(method) ? method : "GET";
+  queryStringObject = typeof queryStringObject === "object" && queryStringObject !== null ? queryStringObject : {};
+  payload = typeof payload === "object" && payload !== null ? payload : {};
+  callback = typeof callback === "function" ? callback : false;
+  headers = typeof headers === "object" && headers !== null ? headers : {};
+
+  // ordering queryStringObject and adding it to path
+  if (Object.keys(queryStringObject).length > 0) {
+    let apiUrl = "";
+    for(let queryKey in queryStringObject) {
+      if (queryStringObject.hasOwnProperty(queryKey)) {
+        if (apiUrl.length === 0)
+          apiUrl = `${path}?${queryKey}=${queryStringObject[queryKey]}`;
+        else
+          apiUrl = `${apiUrl}&${queryKey}=${queryStringObject[queryKey]}`;
+      }
+    }
+
+    path = apiUrl;
+  }
+
+  // Creating request and opening for API call
+  const request = new XMLHttpRequest();
+  request.open(method, path);
+
+  // Adding headers to XMLHttpRequest
+  request.setRequestHeader("Content-Type", "application/json");
+  if (Object.keys(headers).length > 0) {
+    for (const headerKey in headers) {
+      if (headers.hasOwnProperty(headerKey))
+        request.setRequestHeader(headerKey, headers[headerKey]);
+    }
+  }
+
+  // Adding authorization header
+  const { session } = app.config;
+  if (typeof session === "object" && session !== null)
+    request.setRequestHeader("token", session.id);
+
+  // Adding listener as the state of xhr request change
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      const status = request.status;
+      const body = request.statusText;
+
+      if (callback) {
+        try {
+          const parsedData = JSON.parse(body);
+          callback(status, parsedData);
+        } catch (err) {
+          callback(status, false);
+        }
+      }
+    }
+  }
+
+  // Sending request to back end
+  const payloadStringified = JSON.stringify(payload);
+
+  // Sending request
+  request.send(payloadStringified);
+}
